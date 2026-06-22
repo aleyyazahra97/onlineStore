d56ff0ca8734caa6fcda108388c2750600f9f9aa
aleyyazahra97 <aleyyazahra97@gmail.com>
2026-06-22 13:52:15 +0800

    Fix database migration issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// WAJIB ADA INI AGAR WARNA MERAH HILANG:
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        // Gunakan Schema (tanpa backslash) karena sudah di-import di atas
+        Schema::defaultStringLength(191);
+        
+        // Gunakan DB (tanpa backslash) karena sudah di-import di atas
+        if (env('DB_CONNECTION') === 'mysql') {
+            DB::statement('SET SESSION sql_require_primary_key = 0;');
+        }
     }
 }
